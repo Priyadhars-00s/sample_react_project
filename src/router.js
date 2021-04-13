fbb190e1c2bbb7f02edbdac94787ece6c11c5a4c
Priyadhars-00s <priyadh.10decoders@gmail.com>
2021-04-13 14:14:51 +0530

    updated

--- src/router.js
+++ src/router.js
@@ -1,18 +1,47 @@
-import React, {Component} from 'react';
+import React, {useState, Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Link, Switch, Router, Route} from 'react-router-dom';
 import Employee from './employee';
 import Department from './department';
 import Project from './project';
 import Error from './error';
+import Protected from './protected';
+import Unprotected from './unprotected';
+import Home from './homepage';
 
+function App() {
+    const[isAutheticated, setisAutheticated] = useState(false);
+    // onEnter = (e) => {
+    //     if (e.key === 'Enter') {
+    //       e.preventDefault()
+    //     }
+    //   };
+//   onKeyPress={this.onEnter}
+   login = (e) => {
+    e.setisAutheticated(true);
+    console.log("loggedInUser:" + isAutheticated)
+  };
+  onkeypress={login}
 
-
+//   function logout(){
+//     setisAutheticated(false);
+//     console.log("loggedInUser:" + isAutheticated)
+//   }
+// }
+ logout = (e) => {
+    e.setisAutheticated(false);
+    console.log("loggedInUser:" + isAutheticated)
+  };
+  onkeypress={logout}
+  
 class RouterComp extends Component {
     constructor(props){
         super(props)
-        console.log("Props")
+       
+        console.log(this.props)
         }
+        
+        
     render()
     {
         return(
@@ -26,13 +55,35 @@ class RouterComp extends Component {
               <li><Link to="/projects">Projects</Link></li>
             </ul>
                          
-              
+            <ul>
+          <li>
+            <Link to='/'>
+              Link to Home Page
+          </Link>
+          </li>
+          <li>
+            <Link to='/protected'>
+              Link to Protected Page
+          </Link>
+          </li>
+          <li>
+            <Link to='/unprotected'>
+              Link to Unprotected Page
+          </Link>
+          </li>
+        </ul>
+        <button onClick={this.login}>Login</button>
+        <br/>
+        <button onClick={this.logout}>Logout</button>
                 <Switch>
 
               <Route path="/employees" component={this.props.Employee}></Route>
               <Route path="/departments" component={this.props.Department}></Route>
               <Route path="/projects" component={this.props.Project}></Route>
               <Route path="/" Component={Error}></Route>
+              <Route exact path='/' component={Home}/>
+              <Route path='/protected' component={Protected} />
+              <Route path='/unprotected' component={Unprotected} />
               </Switch>
             </Router>
             </div>
@@ -40,5 +91,6 @@ class RouterComp extends Component {
         )
     }
       
+}
 }
 export default RouterComp;
---
+++ src/unprotected.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+function Unprotected(){
+    return(
+        <p>Unprotected Page</p>
+    );
+}
+
+export default Unprotected;
